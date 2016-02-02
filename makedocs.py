961f7831318f016bfc788ad53eb0f0594cc38451
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-02-02 03:44:18 +0300

    Fix buildbot

--- makedocs.py
+++ makedocs.py
@@ -4,13 +4,13 @@
 from buildbot.plugins import *
 from buildbot.steps.source.git import Git
 from buildbot.steps.python import Sphinx
-from buildbot.steps.transfer import FileUpload
 from buildbot.steps.transfer import DirectoryUpload
 from buildbot.changes.gitpoller import GitPoller
 from buildbot.schedulers.basic  import SingleBranchScheduler
 from buildbot.config import BuilderConfig
 from buildbot.steps.master import MasterShellCommand
-from buildbot.steps.shell import WithProperties
+
+import bbconf
 
 repourl = 'git://github.com/nextgis/docs_ng.git'
 
