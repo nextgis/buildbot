83958176081f1533f74c0d1cfb037f911b6b2584
yellow-sky <nikulin.e@gmail.com>
2016-03-23 16:24:59 +0300

    Remake to standard copy command

--- make_ngid_tests.py
+++ make_ngid_tests.py
@@ -3,36 +3,6 @@
 from buildbot.changes.gitpoller import GitPoller
 from buildbot.config import BuilderConfig
 from buildbot.plugins import *
-from buildbot.process import buildstep
-from twisted.internet import defer
-
-
-# CUSTOM BuildSteps (BAD - need to move to separate file)
-class CreateSubConfigCommand(buildstep.ShellMixin, buildstep.BuildStep):
-    name = "create_sub_config"
-
-    config_text = """
-    TEST_ENV = True
-
-    if TEST_ENV:
-        from settings import INSTALLED_APPS
-        import os
-        DJANGO_LIVE_TEST_SERVER_ADDRESS = 'localhost:9200-9300'
-        os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = DJANGO_LIVE_TEST_SERVER_ADDRESS
-        TESTS_BROWSER_DRIVER_NAME = 'phantomjs'  # use acceptable drivers for Splinter
-        INSTALLED_APPS += ('behave_django',)
-    """
-
-    def __init__(self, out_file_path='subconfig.cfg', **kwargs):
-        self.out_file_path = out_file_path
-        kwargs = self.setupShellMixin(kwargs, prohibitArgs=['command'])
-        buildstep.BuildStep.__init__(self, **kwargs)
-
-    @defer.inlineCallbacks
-    def run(self):
-        cmd = yield self.makeRemoteShellCommand(command=['echo "%s" > %s' % (self.config_text, self.out_file_path)])
-        yield self.runCommand(cmd)
-        defer.returnValue(cmd.results())
 
 
 c = {}
@@ -87,10 +57,9 @@ factory.addStep(steps.ShellCommand(name='Install tests requirements',
                                    command=['env/bin/pip', 'install', '-r', 'src/requirements-tests.txt'])
                 )
 
-factory.addStep(CreateSubConfigCommand('src/nextgisid_site/nextgisid_site/settings_local.py',
-                                name='Create test subconfig',
-                                #workdir='build'
-                                )
+factory.addStep(steps.ShellCommand(name='Create tests subconfig',
+                                   workdir='build',
+                                   command=['cp', 'src/nextgisid_site/nextgisid_site/settings_local.py_server_tests_template', 'src/nextgisid_site/nextgisid_site/settings_local.py'])
                 )
 
 factory.addStep(steps.ShellCommand(name='Run behave tests',
