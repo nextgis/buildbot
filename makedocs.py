6da70e034705c3e5005ed78b68530419b6083d3a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-02-20 16:54:55 +0300

    Fix force scheduler for make docs

--- makedocs.py
+++ makedocs.py
@@ -25,12 +25,14 @@ git_poller = GitPoller(project = poller_name,
                    repourl = repourl,
                    workdir = poller_name + '-workdir',
                    branches = langs,
-                   pollinterval = 1800,)
+                   pollinterval = 600,)
 c['change_source'].append(git_poller)
 
+builderNames = []
 for lang in langs:
-    project_name = 'docs_' + lang
-    
+    project_name = poller_name + '_' + lang
+    builderNames.append(project_name)
+
     scheduler = schedulers.SingleBranchScheduler(
                             name=project_name,
                             change_filter=util.ChangeFilter(project = poller_name,
@@ -38,52 +40,53 @@ for lang in langs:
                             treeStableTimer=5*60,
                             builderNames=[project_name])
     c['schedulers'].append(scheduler)
-    c['schedulers'].append(schedulers.ForceScheduler(
-                                name=project_name + "_force",
-                                builderNames=[project_name],
-    ))
+
 
     #### build docs
 
     factory = util.BuildFactory()
     # 1. check out the source
     factory.addStep(steps.Git(repourl=repourl, mode='incremental', submodules=True)) #mode='full', method='clobber'
-    factory.addStep(steps.ShellCommand(command=["sh", "make_javadoc.sh"], 
+    factory.addStep(steps.ShellCommand(command=["sh", "make_javadoc.sh"],
                                       description=["make", "javadoc for mobile (android)"],
-                                      descriptionDone=["made", "javadoc for mobile (android)"], 
+                                      descriptionDone=["made", "javadoc for mobile (android)"],
                                       workdir="build/source/ngmobile_dev"))
-    
+
     # 2. build pdf for each doc except dev
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS Mobile"],
                                       workdir="build/source/docs_ngmobile"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS Web"],
                                       workdir="build/source/docs_ngweb"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS Manager"],
                                       workdir="build/source/docs_ngmanager"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS FormBuilder"],
                                       workdir="build/source/docs_formbuilder"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS Bio"],
                                       workdir="build/source/docs_ngbio"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS QGIS"],
                                       workdir="build/source/docs_ngqgis"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS open geodata portal"],
                                       workdir="build/source/docs_ogportal"))
-    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"], 
+    factory.addStep(steps.ShellCommand(command=["make", "latexpdf"],
                                       description=["make", "pdf for NextGIS forest inspector"],
                                       workdir="build/source/docs_forestinspector"))
     # 3. build html
     factory.addStep(Sphinx(sphinx_builddir="_build/html",sphinx_sourcedir="source",sphinx_builder="html"))
     # 4. upload to ftp
-    factory.addStep(steps.ShellCommand(command=["sync.sh", lang], 
+    factory.addStep(steps.ShellCommand(command=["sync.sh", lang],
                                        description=["sync", "to web server"]))
 
     builder = BuilderConfig(name = project_name, slavenames = ['build-nix'], factory = factory)
     c['builders'].append(builder)
-    
+
+c['schedulers'].append(schedulers.ForceScheduler(
+            name=poller_name + "_force",
+            builderNames=builderNames,
+))
