21bde5338426fdec7a0e3f4cae7dbb3821939973
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-02-20 13:31:22 +0300

    Add document repositories poller

--- updatedocs.py
+++ updatedocs.py
@@ -16,3 +16,58 @@ c = {}
 c['change_source'] = []
 c['schedulers'] = []
 c['builders'] = []
+
+# 'git://github.com/nextgis/docs_howto.git',
+repos = [
+    'docs_howto',
+    'docs_ngcom',
+    'docs_ngmobile',
+    'docs_ngqgis',
+    'docs_ngweb'
+]
+
+main_repourl = 'git://github.com/nextgis/docs_ng.git'
+
+langs = ['ru', 'en']
+
+poller_name = 'updatedocs'
+
+for repo in repos:
+    git_poller = GitPoller(project = poller_name + '_' + repo,
+                       repourl = 'git://github.com/nextgis/' + repo + '.git',
+                       workdir = poller_name + '-' + repo + '-workdir',
+                       branches = langs,
+                       pollinterval = 900,)
+    c['change_source'].append(git_poller)
+
+project_name = 'updatedocs'
+
+scheduler = schedulers.SingleBranchScheduler(
+                    name=project_name,
+                    change_filter=util.ChangeFilter(project = poller_name + '*',
+                                                    branch=langs),
+                    treeStableTimer=2*60,
+                    builderNames=[project_name])
+c['schedulers'].append(scheduler)
+c['schedulers'].append(schedulers.ForceScheduler(
+                            name=project_name + "_force",
+                            builderNames=[project_name],
+))
+
+#### update docs
+
+factory = util.BuildFactory()
+
+factory.addStep(steps.Git(repourl=main_repourl, mode='incremental', submodules=True))
+for lang in langs:
+    factory.addStep(steps.ShellCommand(command=["sh", "switch_lang.sh", lang],
+                                      description=["switch", "language to " + lang],
+                                      descriptionDone=["switched", "language to " + lang],
+                                      workdir="build"))
+    factory.addStep(steps.ShellCommand(command=["sh", "update_docs.sh"], 
+                                      description=["update", lang + " documentation"],
+                                      descriptionDone=["updated", lang + " documentation"],
+                                      workdir="build"))
+
+builder = BuilderConfig(name = project_name, slavenames = ['build-nix'], factory = factory)
+c['builders'].append(builder)
