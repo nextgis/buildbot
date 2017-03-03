5d1221624db18dbcc4578d12ae968ce38f509bfd
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-03-04 00:03:23 +0300

    Update makegdal.py

--- makegdal.py
+++ makegdal.py
@@ -314,8 +314,12 @@ factory_deb.addStep(steps.ShellCommand(command=["dch.py", '-n', project_ver, '-a
                                        description=["tar", "compress"],
                                        descriptionDone=["tar", "compressed"], haltOnFailure=True))
 # copy lib_gdal2 -> debian
-factory_deb.addStep(steps.CopyDirectory(src=deb_dir + "/" + deb_name + "/master/debian", dest=code_dir + "/debian",
-                                        name="add debian folder", haltOnFailure=True))
+factory_deb.addStep(steps.CopyDirectory(src=deb_dir + "/" + deb_name + "/master/debian", 
+                                        dest=code_dir + "/debian",
+                                        name="add debian folder", 
+                                        description=["copy", "debian folder"],
+                                        descriptionDone=["copied", "debian folder"],
+                                        haltOnFailure=True))
 # update changelog
 for ubuntu_distribution in ubuntu_distributions:
     factory_deb.addStep(steps.ShellCommand(command=['dch.py', '-n', project_ver, '-a',
@@ -387,8 +391,12 @@ factory_debdev.addStep(steps.ShellCommand(command=["dch.py", '-n', project_verde
                                        description=["tar", "compress"],
                                        descriptionDone=["tar", "compressed"], haltOnFailure=True))
 # copy lib_gdal2 -> debian
-factory_debdev.addStep(steps.CopyDirectory(src=debdev_dir + "/" + deb_name + "/dev/debian", dest=code_dir + "/debian",
-                                        name="add debian folder", haltOnFailure=True))
+factory_debdev.addStep(steps.CopyDirectory(src=debdev_dir + "/" + deb_name + "/dev/debian", 
+                                           dest=code_dir + "/debian",
+                                           name="add debian folder", 
+                                           description=["copy", "debian folder"],
+                                           descriptionDone=["copied", "debian folder"],
+                                           haltOnFailure=True))
 # update changelog
 for ubuntu_distribution in ubuntu_distributions_dev:
     factory_debdev.addStep(steps.ShellCommand(command=['dch.py', '-n', project_verdev, '-a',
