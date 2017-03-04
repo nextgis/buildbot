529855e283b2896a8fd80ee454a6f2815af5d036
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-03-04 09:49:32 +0300

    NGM3: copy apk to ftp

--- makengm3.py
+++ makengm3.py
@@ -17,6 +17,8 @@ c = {}
 
 repourl = 'git://github.com/nextgis/android_nextgis_mobile.git'
 project_name = 'ngm3'
+ftp = 'ftp://192.168.255.1/'
+myftp = 'ftp://192.168.255.51/'
 
 git_poller = GitPoller(
     project = project_name,
@@ -64,10 +66,10 @@ factory.addStep(steps.RemoveDirectory(dir="build/build"))
 factory.addStep(steps.RemoveDirectory(dir="build/libngui/build"))
 
 factory.addStep(steps.ShellCommand(
-    command=["/bin/bash", "cp", "--remove-destination", "../sentry.properties.ngm3", "build/sentry.properties"],
+    command=["cp", "--remove-destination", "../sentry.properties.ngm3", "build/sentry.properties"],
     name='Copy sentry.properties',
     description=["Copy", "sentry.properties"],
-    descriptionDone=["Copy", "sentry.properties"],
+    descriptionDone=["Copied", "sentry.properties"],
     haltOnFailure=True
 ))
 factory.addStep(steps.ShellCommand(
@@ -78,7 +80,8 @@ factory.addStep(steps.ShellCommand(
     env={
         'ANDROID_HOME': '/opt/android-sdk-linux',
         'ANDROID_NDK_HOME': '/opt/android-sdk-linux/ndk-bundle'
-    }
+    },
+    haltOnFailure=True
 ))
 factory.addStep(steps.ShellCommand(
     command=['dch.py', '-n', 'test', '-a', 'NextGIS Mobile v3', '-p', 'simple',
@@ -91,12 +94,27 @@ factory.addStep(steps.ShellCommand(
 
 # Simlink is needed from testfairy-upload-android-ngm3.sh to virtual env/bin
 # ln -s /full/path/to/testfairy-upload-android-ngm3.sh env/bin/
-factory.addStep(steps.ShellCommand(
-    command=['/bin/bash', 'testfairy-upload-android-ngm3.sh', 'app/build/outputs/apk'],
-    description=["upload", "testfairy"],
-    descriptionDone=["uploaded", "testfairy"],
-    haltOnFailure=True
-))
+#factory.addStep(steps.ShellCommand(
+#    command=['/bin/bash', 'testfairy-upload-android-ngm3.sh', 'app/build/outputs/apk'],
+#    description=["upload", "testfairy"],
+#    descriptionDone=["uploaded", "testfairy"],
+#    haltOnFailure=True
+#))
+
+# Upload apk to ftp
+#ftp_upload_command = "curl -u " + bbconf.ftp_user + " --ftp-create-dirs -T file ftp://nextgis.ru/programs/ngm3/"
+#upld_file_lst = ['app/build/outputs/apk/ngmobile3-' + project_ver + '.apk']
+upld_file_lst = ['app/build/outputs/apk/ngmobile3-0.4.apk']
+for upld_file in upld_file_lst:
+    factory_win.addStep(steps.ShellCommand(
+        command=['curl', '-u', bbconf.ftp_mynextgis_user, '-T', upld_file, '--ftp-create-dirs', myftp + 'ngm3/'],
+        name="upload to ftp",
+        description=["upload", "to ftp " + upld_file],
+        descriptionDone=["uploaded", "NGM3 files to ftp"],
+        haltOnFailure=False,
+        workdir="build/"
+    ))
+
 factory.addStep(steps.ShellCommand(
     command=['dch.py', '-n', 'test', '-a', 'NextGIS Mobile v3', '-p', 'store', '-f', '.'],
     name='log last comments',
