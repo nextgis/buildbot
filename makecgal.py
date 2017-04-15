6c843898e6ae47436ff01147678441ec398c3bb0
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-04-15 21:21:42 +0300

    Update makecgal.py

--- makecgal.py
+++ makecgal.py
@@ -34,7 +34,7 @@ code_dir = 'build/' + code_dir_last
 
 # 1. check out the source
 factory_deb = util.BuildFactory()
-ubuntu_distributions = ['trusty', 'xenial', 'yakkety']
+ubuntu_distributions = ['trusty', 'xenial', 'zesty']
 # 1. check out the source
 deb_name = 'cgal'
 deb_dir = 'build/cgal_deb'
