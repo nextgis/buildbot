55c7ed111f62ee1a5db6985d4f7d6c055fda5fe1
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-04-15 21:23:20 +0300

    Update makegeos.py

--- makegeos.py
+++ makegeos.py
@@ -33,7 +33,7 @@ code_dir = 'build/' + code_dir_last
 
 # 1. check out the source
 factory_deb = util.BuildFactory()
-ubuntu_distributions = ['trusty', 'xenial', 'yakkety']
+ubuntu_distributions = ['trusty', 'xenial', 'zesty']
 # 1. check out the source
 deb_name = 'geos'
 deb_dir = 'build/geos_deb'
