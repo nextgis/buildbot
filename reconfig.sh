951f55215f3ecee579fe109f442e3c209913059d
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-12-19 03:04:32 +0300

    Update reconfig.sh

--- reconfig.sh
+++ reconfig.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# Master monitoring is enough as everything rewrite on git pull
 inotifywait -q -m -e attrib /opt/buildbot/master/master.cfg | while read events; do
     sleep 30
     /opt/buildbot/sandbox/bin/buildbot restart master
 done &
 
-inotifywait -q -m -e attrib /opt/buildbot/master/*.py | while read events; do 
-    sleep 30
-    /opt/buildbot/sandbox/bin/buildbot restart master
-done &
+#inotifywait -q -m -e attrib /opt/buildbot/master/*.py | while read events; do 
+#    sleep 30
+#    /opt/buildbot/sandbox/bin/buildbot restart master
+#done &
