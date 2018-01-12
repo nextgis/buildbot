2cb46fca7cabe153a37169b279ef5ee385c01727
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-01-12 04:00:32 +0300

    Updare refresh.sh

--- reconfig.sh
+++ reconfig.sh
@@ -1,14 +1,14 @@
-t#!/bin/sh
+#!/bin/sh
 
-# Master monitoring is enough as everything rewrite on git pull
-inotifywait -q -m -e modify -e attrib /opt/buildbot/master/master.cfg | while read events; do
-    sleep 15
+# This script restart build bot on master.cfg changes after push to the buildbot
+# repo. Master monitoring is enough as everything rewrite on git pull
+
+inotifywait -e close_write,moved_to,create -m /opt/buildbot/master |
+while read -r directory events filename; do
+  if [ "$filename" = "master.cfg" ]; then
+    sleep 10
     /opt/buildbot/sandbox/bin/buildbot stop /opt/buildbot/master
     sleep 10
     /opt/buildbot/sandbox/bin/buildbot start /opt/buildbot/master
-done &
-
-#inotifywait -q -m -e attrib /opt/buildbot/master/*.py | while read events; do 
-#    sleep 30
-#    /opt/buildbot/sandbox/bin/buildbot restart master
-#done &
+  fi
+done
