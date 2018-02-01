2af8dbfd384e62817e11e2d651ab213a8bf91762
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-02 02:35:55 +0300

    Fix build installer steps

--- make_installer_framework.py
+++ make_installer_framework.py
@@ -19,10 +19,6 @@ ngftp_user = bbconf.ftp_mynextgis_user
 upload_script_src = 'https://raw.githubusercontent.com/nextgis/buildbot/master/ftp_uploader.py'
 upload_script_name = 'ftp_upload.py'
 
-qt_ver_major = 5
-qt_ver_minor = 10
-qt_ver_patch = 0
-
 c['change_source'] = []
 c['schedulers'] = []
 c['builders'] = []
@@ -36,39 +32,42 @@ forceScheduler = schedulers.ForceScheduler(
                                         ])
 c['schedulers'].append(forceScheduler)
 
-# 1. Build openssl static
-openssl_args = ['-DOPENSSL_NO_DYNAMIC_ENGINE=ON', '-DBUILD_STATIC_LIBS=TRUE',
-                '-DSUPPRESS_VERBOSE_OUTPUT=ON', '-DCMAKE_BUILD_TYPE=Release',
-                '-DSKIP_DEFAULTS=ON']
-openssl_git = 'git://github.com/nextgis-borsch/lib_openssl.git'
+# 1. Build qt static
+# 2. Build installer
+# 3. Upload to ftp
+
+#==============================================================================#
+code_dir_last = '{}_code'.format('qt')
+code_dir = os.path.join('build', code_dir_last)
+build_dir = os.path.join(code_dir, 'build')
+
+qt_git = 'git://github.com/nextgis-borsch/lib_qt5.git'
+
+qt_args = [ '-DBUILD_STATIC_LIBS=TRUE', '-DWITH_OpenSSL_EXTERNAL=ON'
+            '-DSUPPRESS_VERBOSE_OUTPUT=ON', '-DCMAKE_BUILD_TYPE=Release',
+            '-DSKIP_DEFAULTS=ON', '-DQT_CONFIGURE_ARGS="-accessibility;-no-opengl;-no-icu;-no-sql-sqlite;-no-qml-debug;-skip;qtactiveqt;-skip;qtlocation;-skip;qtmultimedia;-skip;qtserialport;-skip;qtsensors;-skip;qtxmlpatterns;-skip;qtquickcontrols;-skip;qtquickcontrols2;-skip;qt3d"',
+          ]
 cmake_build = ['cmake', '--build', '.', '--config', 'release', '--']
 
-installer_url = 'git://github.com/nextgis/nextgis_installer.git'
-
-# Windows specific
-win_run_args = list(openssl_args)
+# Windows ##################################################################
+win_run_args = list(qt_args)
 win_cmake_build = list(cmake_build)
 win_cmake_build.append('/m:' + str(vm_cpu_count))
 
-# Mac OS X specific
-mac_run_args = list(openssl_args)
+mac_run_args = list(qt_args)
 mac_cmake_build = list(cmake_build)
 mac_run_args.extend(['-DCMAKE_OSX_SYSROOT=' + mac_os_sdks_path + '/MacOSX.sdk', '-DCMAKE_OSX_DEPLOYMENT_TARGET=' + max_os_min_version])
 mac_cmake_build.append('-j' + str(vm_cpu_count))
 
-code_dir_last = '{}_code'.format('openssl')
-code_dir = os.path.join('build', code_dir_last)
-build_dir = os.path.join(code_dir, 'build')
-open_ssl_include1 = os.path.join(code_dir, 'include')
-open_ssl_include2 = os.path.join(build_dir, 'include')
-open_ssl_lib = os.path.join(build_dir, 'release')
-
-# Windows ##################################################################
-
 factory_win = util.BuildFactory()
+factory_mac = util.BuildFactory()
 # Install common dependencies
 
-factory_win.addStep(steps.Git(repourl=openssl_git,
+factory_win.addStep(steps.Git(repourl=qt_git,
+                              mode='full',
+                              submodules=False,
+                              workdir=code_dir))
+factory_mac.addStep(steps.Git(repourl=qt_git,
                                 mode='full',
                                 submodules=False,
                                 workdir=code_dir))
@@ -76,6 +75,8 @@ factory_win.addStep(steps.Git(repourl=openssl_git,
 # make build dir
 factory_win.addStep(steps.MakeDirectory(dir=build_dir,
                                         name="Make directory 32 bit"))
+factory_mac.addStep(steps.MakeDirectory(dir=build_dir,
+                                        name="Make directory"))
 
 # configure view cmake
 factory_win.addStep(steps.ShellCommand(command=["cmake", win_run_args, '-G', 'Visual Studio 15 2017', '../'],
@@ -84,111 +85,12 @@ factory_win.addStep(steps.ShellCommand(command=["cmake", win_run_args, '-G', 'Vi
                                        descriptionDone=["cmake", "configured for win32"],
                                        haltOnFailure=True,
                                        workdir=build_dir))
-
-# make
-factory_win.addStep(steps.ShellCommand(command=win_cmake_build,
-                                       name="make 32 bit",
-                                       description=["cmake", "make for win32"],
-                                       descriptionDone=["cmake", "made for win32"],
-                                       haltOnFailure=True,
-                                       workdir=build_dir))
-
-# 2. Build qt5 static minimal
-
-code_dir_last = '{}_code'.format('qt')
-code_dir = os.path.join('build', code_dir_last)
-build_dir = os.path.join(code_dir, 'build')
-
-# make build dir
-factory_win.addStep(steps.MakeDirectory(dir=code_dir,
-                                        name="Make qt directory"))
-
-qt_version = '{}.{}.{}'.format(qt_ver_major, qt_ver_minor, qt_ver_patch)
-qt_subfolder = 'single/'
-qt_input_name = 'qt-everywhere-src-{}'.format(qt_version)
-qt_url = 'http://download.qt.io/official_releases/qt/{}.{}/{}/{}{}.tar.xz'.format(qt_ver_major, qt_ver_minor, qt_version, qt_subfolder, qt_input_name)
-
-qt_args = [ '-release', '-static', '-opensource', '-confirm-license',
-            '-accessibility', '-no-opengl', '-no-icu', '-no-sql-sqlite',
-            '-no-qml-debug', '-nomake', 'examples', '-nomake', 'tests', '-skip',
-            'qtactiveqt', '-skip', 'qtlocation', '-skip', 'qtmultimedia', '-skip',
-            'qtserialport', '-skip', 'qtsensors', '-skip', 'qtxmlpatterns',
-            '-skip', 'qtquickcontrols', '-skip', 'qtquickcontrols2', '-skip',
-            'qt3d', '-openssl-linked']
-
-factory_win.addStep(steps.ShellCommand(command=["curl", qt_url, '-o', 'qt.tar.xz', '-s', '-L'],
-                                       name="download qt",
-                                       description=["curl", "download qt"],
-                                       descriptionDone=["curl", "downloaded qt"],
-                                       haltOnFailure=True,
-                                       workdir=code_dir))
-
-factory_win.addStep(steps.ShellCommand(command=["cmake", '-E', 'tar', 'xzf', 'qt.tar.xz'],
-                                       name="extract qt",
-                                       description=["cmake", "extract qt"],
-                                       descriptionDone=["cmake", "extracted qt"],
-                                       haltOnFailure=True,
-                                       workdir=code_dir))
-
-factory_win.addStep(steps.ShellCommand(command=["configure", '-prefix', '%CD%\\qtbase',
-                                                '-platform', 'win32-msvc2017', qt_args,
-                                                '-I', open_ssl_include1, '-I', open_ssl_include2,
-                                                '-L', open_ssl_lib, '-l', 'Gdi32', '-l', 'User32'],
-                                       name="configure qt",
-                                       description=["configure", "qt"],
-                                       descriptionDone=["configure", "qt"],
-                                       haltOnFailure=True,
-                                       workdir=os.path.join(code_dir, qt_input_name)))
-
-factory_win.addStep(steps.ShellCommand(command=["nmake"],
-                                       name="make qt",
-                                       description=["make", "qt"],
-                                       descriptionDone=["made", "qt"],
-                                       haltOnFailure=True,
-                                       workdir=os.path.join(code_dir, qt_input_name)))
-
-# 3. Build installer framework
-code_dir_last = '{}_code'.format('installer')
-code_dir = os.path.join('build', code_dir_last)
-build_dir = os.path.join(code_dir, 'build')
-
-factory_win.addStep(steps.Git(repourl=installer_git,
-                                mode='full',
-                                submodules=False,
-                                workdir=code_dir))
-
-# qtifw/tools/build_installer.py                                
-
-# 4. Upload installer framework to ftp
-
-builder_win = util.BuilderConfig(name = project_name + '_win', workernames = ['build-win'], factory = factory_win)
-
-c['builders'].append(builder_win)
-
-# MacOS X ##################################################################
-code_dir_last = '{}_code'.format('openssl')
-code_dir = os.path.join('build', code_dir_last)
-build_dir = os.path.join(code_dir, 'build')
-
-factory_mac = util.BuildFactory()
-
-factory_mac.addStep(steps.Git(repourl=openssl_git,
-                                mode='full',
-                                submodules=False,
-                                workdir=code_dir))
-
 env = {
     'PATH': [
                 "/usr/local/bin",
                 "${PATH}"
             ],
 }
-
-# make build dir
-factory_mac.addStep(steps.MakeDirectory(dir=build_dir,
-                                        name="Make directory"))
-
-# configure view cmake
 factory_mac.addStep(steps.ShellCommand(command=["cmake", mac_run_args, '..'],
                                        name="configure",
                                        description=["cmake", "configure"],
@@ -198,6 +100,12 @@ factory_mac.addStep(steps.ShellCommand(command=["cmake", mac_run_args, '..'],
                                        env=env))
 
 # make
+factory_win.addStep(steps.ShellCommand(command=win_cmake_build,
+                                       name="make 32 bit",
+                                       description=["cmake", "make for win32"],
+                                       descriptionDone=["cmake", "made for win32"],
+                                       haltOnFailure=True,
+                                       workdir=build_dir))
 factory_mac.addStep(steps.ShellCommand(command=mac_cmake_build,
                                        name="make",
                                        description=["cmake", "make"],
@@ -206,6 +114,26 @@ factory_mac.addStep(steps.ShellCommand(command=mac_cmake_build,
                                        workdir=build_dir,
                                        env=env))
 
+# 2. Build installer framework
+code_dir_last = '{}_code'.format('installer')
+code_dir = os.path.join('build', code_dir_last)
+build_dir = os.path.join(code_dir, 'build')
+
+factory_win.addStep(steps.Git(repourl=installer_git,
+                                mode='full',
+                                submodules=False,
+                                workdir=code_dir))
+factory_mac.addStep(steps.Git(repourl=installer_git,
+                                mode='full',
+                                submodules=False,
+                                workdir=code_dir))
+
+# qtifw/tools/build_installer.py
+
+# 3. Upload installer framework to ftp
+
+builder_win = util.BuilderConfig(name = project_name + '_win', workernames = ['build-win'], factory = factory_win)
 builder_mac = util.BuilderConfig(name = project_name + '_mac', workernames = ['build-mac'], factory = factory_mac)
 
+c['builders'].append(builder_win)
 c['builders'].append(builder_mac)
