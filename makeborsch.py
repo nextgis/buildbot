e5fd2e6f45009afc0e9f2ebd83c160941b23e9d7
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-18 03:06:39 +0300

    Add expat

--- makeborsch.py
+++ makeborsch.py
@@ -15,6 +15,7 @@ repositories = [
     {'repo':'lib_gif', 'args':[], 'requirements':[]},
     {'repo':'lib_geos', 'args':[], 'requirements':[]},
     {'repo':'lib_qhull', 'args':[], 'requirements':[]},
+    {'repo':'lib_expat', 'args':[], 'requirements':[]},
     {'repo':'lib_png', 'args':['-DWITH_ZLIB=ON', '-DWITH_ZLIB_EXTERNAL=ON'], 'requirements':[]},
     {'repo':'lib_freetype', 'args':['-DWITH_ZLIB=ON', '-DWITH_ZLIB_EXTERNAL=ON', '-DWITH_PNG=ON', '-DWITH_PNG_EXTERNAL=ON'], 'requirements':[]},
     {'repo':'lib_agg', 'args':['-DWITH_Freetype=ON', '-DWITH_Freetype_EXTERNAL=ON'], 'requirements':[]},
