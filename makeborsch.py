58da50c9688e6fb1896b3084ea7429bba3b1b899
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-27 00:48:11 +0300

    Add lib_jbig

--- makeborsch.py
+++ makeborsch.py
@@ -17,6 +17,7 @@ repositories = [
     {'repo':'lib_qhull', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_expat', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_jsonc', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
+    {'repo':'lib_jbig', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_opencad', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_jpeg', 'args':['-DBUILD_TESTING=ON', '-DBUILD_JPEG_12=ON', '-DBUILD_JPEG_8=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_proj', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
