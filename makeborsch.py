b93f817a1f1222c49239b9b4b7d01d7c042714ce
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-06 12:28:23 +0300

    Add lib_yaml

--- makeborsch.py
+++ makeborsch.py
@@ -18,6 +18,7 @@ repositories = [
     {'repo':'lib_expat', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_jsonc', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_gsl', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
+    {'repo':'lib_yaml', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_jbig', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_szip', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_opencad', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
