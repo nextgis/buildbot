0b4dfbc8e9a11703a43da5163e1b920a224a5bbf
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-07 03:01:52 +0300

    Add numpy

--- makeborsch.py
+++ makeborsch.py
@@ -16,6 +16,7 @@ repositories = [
     {'repo':'lib_geos', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_qhull', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_expat', 'args':[], 'requirements':[], 'skip':[]},
+    {'repo':'numpy', 'args':[], 'requirements':[], 'skip':[]},
     {'repo':'lib_jsonc', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_gsl', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
     {'repo':'lib_yaml', 'args':['-DBUILD_TESTING=ON'], 'requirements':[], 'skip':[]},
