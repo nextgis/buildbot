aefa0ddc398c554fbcd9a7f4d3b6629f93878339
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-11-27 10:32:41 +0300

    Update ngstd version

--- makedeb.py
+++ makedeb.py
@@ -20,7 +20,7 @@ repositories = [
     {'repo':'dante','version':'1.4.2', 'deb':'dante', 'subdir': '', 'org':'nextgis', 'url': ''},
     {'repo':'pam-pgsql','version':'0.7.3.3', 'deb':'pam-pgsql', 'subdir': '', 'org':'nextgis', 'url': ''},
     {'repo':'nextgisqgis','version':'18.10.0', 'deb':'nextgisqgis', 'subdir': '', 'org':'nextgis', 'url': ''},
-    {'repo':'lib_ngstd','version':'0.9.0', 'deb':'ngstd', 'subdir': '', 'org':'nextgis', 'url': ''},
+    {'repo':'lib_ngstd','version':'0.9.1', 'deb':'ngstd', 'subdir': '', 'org':'nextgis', 'url': ''},
     {'repo':'formbuilder','version':'2.2', 'deb':'formbuilder', 'subdir': '', 'org':'nextgis', 'url': ''},
     {'repo':'protobuf-c','version':'1.3.0', 'deb':'protobuf-c', 'subdir': '', 'org':'nextgis-borsch', 'url': ''},
     {'repo':'protobuf','version':'3.5.1', 'deb':'protobuf', 'subdir': '', 'org':'nextgis-borsch', 'url': ''},
