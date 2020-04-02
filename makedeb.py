fd6f9c365d852b7e72a4f32060fb6286ea2cc957
Dmitry Baryshnikov <bishop.dev@gmail.com>
2020-04-02 03:08:00 +0300

    change add repo

--- makedeb.py
+++ makedeb.py
@@ -100,9 +100,9 @@ for repository in repositories:
         factory.addStep(steps.ShellSequence(commands=[
                 util.ShellArg(command=["curl", script_src, '-o', script_name, '-s', '-L'], 
                 logfile=logfile),
-                util.ShellArg(command=["echo", 'deb https://rm.nextgis.com/api/repo/{}/deb {} main'.format(repository['repo_id'], platform['name']), '|', 'tee', '-a', '/etc/apt/sources.list'], 
+                util.ShellArg(command=["echo", 'deb https://rm.nextgis.com/api/repo/{}/deb {} main'.format(repository['repo_id'], platform['name']), '>>', '/etc/apt/sources.list'], 
                 logfile=logfile),
-                util.ShellArg(command=["curl", '-s', '-L', 'https://rm.nextgis.com/api/repo/{}/deb/key.gpg'.format(repository['repo_id']), '|', 'apt-key', 'add', '-'], 
+                util.ShellArg(command=["apt-key", 'adv', '--fetch-keys', 'https://rm.nextgis.com/api/repo/{}/deb/key.gpg'.format(repository['repo_id'])], 
                 logfile=logfile),
                 util.ShellArg(command=["apt", 'update'], 
                 logfile=logfile),
