0285084a246e884739b6fb74861d7a15b79aa563
d3n15 <x514ckw4r3@gmail.com>
2022-05-02 16:21:03 +0300

    qgis_headless repository changed

--- makedeb.py
+++ makedeb.py
@@ -39,7 +39,7 @@ repositories = [
         },]
     },
     {'repo':'nextgisqgis', 'deb':'nextgisqgis', 'repo_root':'https://github.com', 'org': 'nextgis', 'os': ['bionic', 'focal', 'buster', 'bullseye', 'jammy',], 'repo_id':11, 'branch': 'dev/up_to_3'},
-    {'repo':'qgis_headless', 'deb':'qgis-headless', 'repo_root':'https://gitlab.com:' + os.environ.get("BUILDBOT_APITOKEN_GITLAB_FULL") + '@gitlab.com', 'org': 'nextgis_private', 'os': ['bionic', 'focal', 'buster','bullseye', 'jammy',], 'repo_id':11, 'branch': 'master'},
+    {'repo':'qgis_headless', 'deb':'qgis-headless', 'repo_root':'https://github.com', 'org': 'nextgis', 'os': ['bionic', 'focal', 'buster','bullseye', 'jammy',], 'repo_id':11, 'branch': 'master'},
     {'repo':'terratile', 'deb':'python-terratile', 'repo_root':'https://github.com', 'org': 'nextgis', 'os':['bionic', 'bullseye', 'focal', 'buster', 'jammy'], 'repo_id':11, 'branch': 'gdal3'},
     # {'repo':'lib_qscintilla', 'version':'2.10.4', 'deb':'qscintilla', 'subdir': '', 'repo_root':'nextgis-borsch', 'url': '', 'ubuntu_distributions': ['trusty', 'focal', 'bionic']},
     # {'repo':'py_future', 'version':'0.17.1', 'deb':'python-future', 'subdir': '', 'repo_root':'nextgis-borsch', 'url': 'https://files.pythonhosted.org/packages/90/52/e20466b85000a181e1e144fd8305caf2cf475e2f9674e797b222f8105f5f/future-0.17.1.tar.gz', 'ubuntu_distributions': ['trusty', 'focal', 'bionic']},
