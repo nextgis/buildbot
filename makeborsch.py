835354e4c192f41943cbe3acb53d23a8c39faca7
d3n15 <x514ckw4r3@gmail.com>
2023-02-07 23:52:45 +0300

    Added py_rapidfuzz

--- makeborsch.py
+++ makeborsch.py
@@ -89,6 +89,7 @@ repositories = [
     {'repo':'cert_importer', 'args':['-DWITH_Qt5_EXTERNAL=ON'], 'requirements':[], 'os':['win64'], 'repo_root':'https://github.com', 'org':'nextgis', 'test_regex':[]},
     {'repo':'py_pandas', 'args':['-DWITH_NUMPY_EXTERNAL=ON'], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
     {'repo':'py_levenshtein', 'args':[], 'requirements':['scikit-build'], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
+    {'repo':'py_rapidfuzz', 'args':[], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
 ]
 
 #skip_send2github = [
