9921518690449356a51fb46590b11e2efd708eea
d3n15 <x514ckw4r3@gmail.com>
2023-02-07 15:20:54 +0300

    Added WITH_NUMPY_EXTERNAL=ON to py_pandas

--- makeborsch.py
+++ makeborsch.py
@@ -87,7 +87,7 @@ repositories = [
     {'repo':'py_packaging', 'args':[], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
     {'repo':'py_plotly', 'args':[], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
     {'repo':'cert_importer', 'args':['-DWITH_Qt5_EXTERNAL=ON'], 'requirements':[], 'os':['win64'], 'repo_root':'https://github.com', 'org':'nextgis', 'test_regex':[]},
-    {'repo':'py_pandas', 'args':[], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
+    {'repo':'py_pandas', 'args':['-DWITH_NUMPY_EXTERNAL=ON'], 'requirements':[], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
     {'repo':'py_levenshtein', 'args':[], 'requirements':['scikit-build'], 'os':['win64','mac'], 'repo_root':'https://github.com', 'org':'nextgis-borsch', 'test_regex':[]},
 ]
 
