b2864f885fa9f223d62fb82aa4e13d38f9438cec
x514ckw4r3 <x514ckw4r3@gmail.com>
2025-11-21 00:09:11 +0300

    Revert "Increase timeout for test"
    This reverts commit e8a62414d64b4d7cb4ebc60c74c025bce80b9c86.

--- nextgis_buildbot/scenarios/windows/create_installer.py
+++ nextgis_buildbot/scenarios/windows/create_installer.py
@@ -22,8 +22,8 @@ username = "buildbot"  # username = 'bishopgis'
 https_proxy = os.environ.get("BUILDBOT_HTTPS_PROXY")
 installer_git = "https://github.com/nextgis/nextgis_installer.git"
 
-timeout = 18000
-max_time = 24000
+timeout = 180
+max_time = 240
 
 c = {}
 c["change_source"] = []
