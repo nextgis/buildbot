2fc8c43aadbed55be96aa01fec4dd8ddde81b672
x514ckw4r3 <x514ckw4r3@gmail.com>
2025-11-21 20:50:09 +0300

    Revert "timeout = 0"
    This reverts commit 7e380f0c392c064559d0a937631eb22b8ce433b8.

--- nextgis_buildbot/scenarios/windows/create_installer.py
+++ nextgis_buildbot/scenarios/windows/create_installer.py
@@ -593,7 +593,7 @@ for platform in platforms:
             ],
             name="Prepare packages data",
             maxTime=max_time * 60,
-            timeout=0,
+            timeout=timeout * 60,
             haltOnFailure=True,
             workdir=code_dir,
             env=env_proxy,
